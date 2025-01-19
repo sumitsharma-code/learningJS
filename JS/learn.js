60d2ae3f4dff3bf878236ca1432960ecd2dfb8ce
Sumitsha11 <97402908+sumitsharma-code@users.noreply.github.com>
2025-01-19 23:11:16 +0530

    Basics of JS

---
+++ JS/learn.js
@@ -0,0 +1,120 @@
+// var l = "Sumit";
+// var ln = "Sharma";
+// console.log("Good day! " + l +" "+ln);
+
+// const gender = "a";
+
+// if(gender=="Male")
+// {
+//     console.log("Hey, Bro!");
+// }
+// else if(gender=="Female")
+// {
+//     console.log("How r u doin'?");
+// }
+// else
+// {
+//     console.log("Not a valid Gender!");
+// }
+
+// for (let i = 1; i < 1001; i++) {
+//   console.log(i + " ");
+// }
+
+// const gen = "Ma";
+
+// if(gen == "M")
+// {
+// 	console.log(gen);
+// }
+// else if (gen == "F")
+// {
+// 	console.log("F");
+// }
+// else
+// {
+//     console.log("Not a valid gender!");
+// }
+// let ans = 0;
+// for (let i = 1; i < 1001; i++) {
+//     ans+=i;
+// }
+// console.log(ans);
+
+// const arr = [1, 2, 6, 62, 666];
+// console.log(arr.length);
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// const arr = [1, 2, 6568, 62, 666];
+// let ans = -1;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > ans) {
+//     ans = arr[i];
+//   }
+// }
+// console.log(ans);
+
+// const arrName = ["sumit", "revi", "aksh"];
+// const arrSname = ["sharma", "kaur", "singh"];
+// const arrSex = ["male", "female", "male"];
+
+// let n = arrName.length;
+
+// for(let i = 0; i < n;i++)
+// {
+//   if(arrSex[i] == "male")
+//   {
+//     console.log(arrName[i]);
+//   }
+// }
+
+// const ar = [
+//   {
+//     na: "Sumit",
+//     gender: "Male",
+//   },
+//   {
+//     na: "revi",
+//     gender: "Female",
+//   },
+//   {
+//     na: "aksh",
+//     gender: "Male",
+//   },
+//   {
+//     na: "rahul",
+//     gender: "female",
+//   },
+// ];
+
+// for(let i = 0;i < ar.length;i++)
+// {
+//   if(ar[i]["gender"] == "Male")
+//   {
+//     console.log(ar[i]["na"]);
+//   }
+// }
+
+// const arrName = ["sumit", "revi", "aksh", "shabad", "Greet","hehe","lala"];
+
+// for (let i = 0; i < arrName.length / 2; i++) {
+//   let a = arrName[i];
+//   arrName[i] = arrName[arrName.length - 1 - i];
+//   arrName[arrName.length - 1 - i] = a;
+// }
+
+// arrName.forEach(nam => console.log(nam));
+
+// function findSum(a,b)
+// {
+//   return a+b;
+// }
+
+// console.log(findSum(1,5));
+
